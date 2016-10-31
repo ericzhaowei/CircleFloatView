ad70508f3a460b6a9f454ddb5445df0151b66bc3
eric <zwi_1727@163.com>
2016-10-31 18:35:22 +0800

    add text width attribute

--- app/src/main/java/com/ider/circlefloatview/FloatImage.java
+++ app/src/main/java/com/ider/circlefloatview/FloatImage.java
@@ -1,5 +1,6 @@
 package com.ider.circlefloatview;
 
+import android.animation.Animator;
 import android.animation.ValueAnimator;
 import android.content.Context;
 import android.content.res.TypedArray;
@@ -12,40 +13,53 @@ import android.util.Log;
 import android.view.animation.LinearInterpolator;
 import android.widget.ImageView;
 
+import com.ider.circlefloatview.R;
+
 /**
  * Created by ider-eric on 2016/10/27.
  */
 
 public class FloatImage extends ImageView {
 
-    private int radius = 100;
+    private int DEFAULT_RADIUS = 33;  //dp
+    private int DEFAULT_IMAGE_WIDTH = 0; //dp
+    private int DEFAULT_IMAGE_HEIGHT = 0; //dp
+    private int DEFAULT_TEXT_COLOR = 0x000000;
+    private int DEFAULT_TEXT_SIZE = 13; //sp
+
+    private int mRadius;
+    private int mImageWidth;
+    private int mImageHeight;
+    private int mTextColor;
+    private int mTextSize;
+    private int mTextWidth;
+
     private ValueAnimator animator;
-    private int imageWidth, imageHeight;
     private String text;
     private Paint textPaint;
-    private Rect textBounds;
-    private int textColor;
-    private int textSize;
 
     public FloatImage(Context context) {
         super(context);
         setupAnimator();
+
     }
 
     public FloatImage(Context context, AttributeSet attrs) {
         super(context, attrs);
         TypedArray array = context.obtainStyledAttributes(attrs, R.styleable.FloatImage);
-        radius = array.getDimensionPixelSize(R.styleable.FloatImage_radius, 100);
-        imageWidth = array.getDimensionPixelSize(R.styleable.FloatImage_image_width, 0);
-        imageHeight = array.getDimensionPixelSize(R.styleable.FloatImage_image_height, 0);
+        mRadius = (int) array.getDimension(R.styleable.FloatImage_radius, DimenUtil.dp2px(context.getResources(), DEFAULT_RADIUS));
+        mImageWidth = (int) array.getDimension(R.styleable.FloatImage_image_width, DimenUtil.dp2px(context.getResources(), DEFAULT_IMAGE_WIDTH));
+        mImageHeight = (int) array.getDimension(R.styleable.FloatImage_image_height, DimenUtil.dp2px(context.getResources(), DEFAULT_IMAGE_HEIGHT));
 
         int textId = array.getResourceId(R.styleable.FloatImage_text, 0);
         if(textId != 0) {
             text = getResources().getString(textId);
-            textColor = array.getColor(R.styleable.FloatImage_textColor, 0x000000);
-            textSize = array.getDimensionPixelSize(R.styleable.FloatImage_textSize, 40);
+            mTextColor = array.getColor(R.styleable.FloatImage_textColor, DEFAULT_TEXT_COLOR);
+            mTextSize = (int) array.getDimension(R.styleable.FloatImage_textSize, DimenUtil.sp2px(context.getResources(), DEFAULT_TEXT_SIZE));
+            mTextWidth = (int) array.getDimension(R.styleable.FloatImage_textWidth, mImageWidth + 2*mRadius);
             setupTextPaint();
         }
+        array.recycle();
         setupAnimator();
 
     }
@@ -53,11 +67,8 @@ public class FloatImage extends ImageView {
     private void setupTextPaint() {
         textPaint = new Paint();
         textPaint.setTypeface(Typeface.DEFAULT_BOLD);
-        Log.i("tag", "size=" + textSize);
-        textPaint.setTextSize(textSize);
-        textPaint.setColor(textColor);
-        textBounds = new Rect();
-        textPaint.getTextBounds(text, 0, text.length(), textBounds);
+        textPaint.setTextSize(mTextSize);
+        textPaint.setColor(mTextColor);
     }
 
     private void setupAnimator() {
@@ -66,13 +77,16 @@ public class FloatImage extends ImageView {
         animator.setDuration(1000);
         animator.setInterpolator(new LinearInterpolator());
         animator.addUpdateListener(animatorUpdateListener);
+        animator.addListener(animatorListener);
         animator.setRepeatCount(ValueAnimator.INFINITE);
+        startFloat();
     }
 
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
-
-        setMeasuredDimension(imageWidth + 2*radius, imageHeight + 2*radius + textBounds.height());
+        int width = Math.max(mImageWidth + 2*mRadius, mTextWidth);
+        int height = (int) (mImageHeight + 2*mRadius + textPaint.descent() - textPaint.ascent());
+        setMeasuredDimension(width, height);
     }
 
     @Override
@@ -83,11 +97,12 @@ public class FloatImage extends ImageView {
             return;
         }
 
-        int width = textBounds.width();
-        int height = textBounds.height();
 
-        int x = (getWidth() - width) / 2;
-        int y = getHeight() - 5;
+        float width = textPaint.measureText(text);
+        float height = textPaint.ascent() + textPaint.descent();
+
+        int x = (int) ((getWidth() - width) / 2);
+        int y = getHeight() - (int)textPaint.descent();
 
         canvas.drawText(text, 0, text.length(), x, y, textPaint);
 
@@ -98,23 +113,52 @@ public class FloatImage extends ImageView {
         public void onAnimationUpdate(ValueAnimator valueAnimator) {
             float value = (float) valueAnimator.getAnimatedValue();
 
-            float left = radius * (1 + (float) Math.sin(Math.toRadians(value)));
-            float top = radius * (1 - (float) Math.cos(Math.toRadians(value)));
-            float right = getWidth() - imageWidth - left;
-            float bottom = getHeight() - imageHeight - top;
+            // 该值表示默认的左padding，其值为(getWidth() - (mImageWidth + 2*mRadius)) / 2
+            // 当textWidth等于mImageWidth + 2*mRadius时，该值为0
+            float leftBase = (getWidth() - (mImageWidth + 2*mRadius)) / 2;
+            float left = leftBase + mRadius * (1 + (float) Math.sin(Math.toRadians(value)));
+            float top = mRadius * (1 - (float) Math.cos(Math.toRadians(value)));
+            float right = getWidth() - mImageWidth - left;
+            float bottom = getHeight() - mImageHeight - top;
             setPadding((int) left, (int) top, (int) right, (int) bottom);
-            invalidate();
+        }
+    };
+
+    Animator.AnimatorListener animatorListener = new Animator.AnimatorListener() {
+        @Override
+        public void onAnimationStart(Animator animation) {
+
+        }
+
+        @Override
+        public void onAnimationEnd(Animator animation) {
+
+        }
+
+        @Override
+        public void onAnimationCancel(Animator animation) {
+            int left = (getWidth() - mImageWidth) / 2;
+            int top = (getHeight() - mImageHeight) / 2;
+            setPadding(left, top, left, top);
+        }
+
+        @Override
+        public void onAnimationRepeat(Animator animation) {
+
         }
     };
 
 
-    @Override
-    protected void onAttachedToWindow() {
-        super.onAttachedToWindow();
-        startFloat();
-    }
-
     public void startFloat() {
         animator.start();
     }
+
+    public void stopFloat() {
+        animator.cancel();
+    }
+
+    public void setText(int resId) {
+        text = getResources().getString(resId);
+    }
+
 }
